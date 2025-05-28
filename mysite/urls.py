ffd674c10ea424bd3922aa8e6c39755d7d39a74f
Владислава Пивницкая <vlada.ono@MacBook-Air-Vladislava.local>
2025-05-28 11:44:50 +0300

    Пункт 6

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from xgate import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('viexam/', views.viexam_view, name='viexam'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- xgate/views.py
+++ xgate/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import viexam
 
+
+def viexam_view(request):
+    exams = viexam.objects.filter(is_public=True)  
+    context = {
+        'exams': exams,
+        'fio': 'Пивницкая Владислава Игоревна',
+        'group': '241-672',
+    }
+    return render(request, 'xgate/exam.html', context)
 # Create your views here.
